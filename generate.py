c4990a314fbece39dfdd552e11f7a88fd3f862c0
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2023-12-13 12:01:10 +0000

    New generate.py

---
+++ generate.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+
+from qsc import Qsc
+from qsc.util import mu0, fourier_minimum
+
+# -----------------------------------------------------------------------------
+# set up warning behavior, turn warnings into exceptions
+
+import warnings
+warnings.filterwarnings('error')
+
+from qsc.newton import logger
+def warning(msg, *args, **kwargs):
+    raise Warning(msg)
+logger.warning = warning
+
+# -----------------------------------------------------------------------------
+
+fname = 'dataset.csv'
+f = open(fname, 'w')
+
+fields = ['rc1', 'rc2', 'rc3', 'zs1', 'zs2', 'zs3', 'nfp', 'etabar', 'B2c', 'p2',
+          'iota', 'max_elongation', 'min_L_grad_B', 'min_R0', 'r_singularity',
+          'L_grad_grad_B', 'B20_variation', 'beta', 'DMerc_times_r2']
+
+print(','.join(fields))
+print(','.join(fields), file=f)
+
+while True:
+
+    rc1 = np.random.choice([-1., 1.]) * np.random.uniform(0., 1.)
+    rc2 = np.random.choice([-1., 1.]) * np.random.uniform(0., np.fabs(rc1))
+    rc3 = np.random.choice([-1., 1.]) * np.random.uniform(0., np.fabs(rc2))
+
+    zs1 = np.random.choice([-1., 1.]) * np.random.uniform(0., 1.)
+    zs2 = np.random.choice([-1., 1.]) * np.random.uniform(0., np.fabs(zs1))
+    zs3 = np.random.choice([-1., 1.]) * np.random.uniform(0., np.fabs(zs2))
+
+    rc = [1., rc1, rc2, rc3]
+    zs = [0., zs1, zs2, zs3]
+
+    nfp = np.random.randint(1, 11)
+
+    etabar = np.random.choice([-1., 1.]) * np.random.uniform(0.01, 3.)
+
+    B2c = np.random.choice([-1., 1.]) * np.random.uniform(0.01, 3.)
+
+    p2 = (-1.) * np.random.uniform(0., 4e6)
+
+    order = 'r2'
+
+    try:
+        stel = Qsc(rc=rc, zs=zs, nfp=nfp, etabar=etabar, B2c=B2c, p2=p2, order=order)
+
+        iota           = stel.iota
+        max_elongation = stel.max_elongation
+        min_L_grad_B   = stel.min_L_grad_B
+        min_R0         = stel.min_R0
+        r_singularity  = stel.r_singularity
+        L_grad_grad_B  = fourier_minimum(stel.L_grad_grad_B)
+        B20_variation  = stel.B20_variation
+        beta           = -mu0 * p2 * stel.r_singularity**2 / stel.B0**2
+        DMerc_times_r2 = stel.DMerc_times_r2
+
+#        assert iota >= 0.2
+#        assert max_elongation <= 10.
+#        assert min_L_grad_B >= 0.1
+#        assert min_R0 >= 0.3
+#        assert r_singularity >= 0.05
+#        assert L_grad_grad_B >= 0.1
+#        assert B20_variation <= 5.
+#        assert beta >= 1e-4
+#        assert DMerc_times_r2 > 0.
+
+        values = [rc1, rc2, rc3, zs1, zs2, zs3, nfp, etabar, B2c, p2,
+                  iota, max_elongation, min_L_grad_B, min_R0, r_singularity,
+                  L_grad_grad_B, B20_variation, beta, DMerc_times_r2]
+        
+        print(','.join([str(val) for val in values]))
+        print(','.join([str(val) for val in values]), file=f)
+
+    except Warning:
+        continue
+
+    except AssertionError:
+        continue
+
+    except KeyboardInterrupt:
+        break
+
+f.close()
+
+# -----------------------------------------------------------------------------
+# try reading the file, to check how the results will look like
+
+df = pd.read_csv(fname)
+if len(df) > 0:
+    print(df)
+
+# -----------------------------------------------------------------------------
