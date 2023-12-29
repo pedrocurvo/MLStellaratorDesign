a76c2714062078efb7d58dfa48147de4f3771df7
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2023-12-29 19:28:01 +0000

    Adapt test to object with restrictions

--- test.py
+++ test.py
@@ -1,25 +1,31 @@
 from StellatorsDataSet import StellatorsDataSet
-import torch
-from torch.utils.data import DataLoader
-from torch import nn
-from torch import optim
 import numpy as np
-from sklearn.impute import SimpleImputer
 
+
+# -----------------------------------------------------------------------------
 # Load the dataset
 dataset = StellatorsDataSet('data/dataset.npy')  
 print(f'Number of samples: {len(dataset)}')
 print(f'Number of features: {dataset.features.shape[1]}')
 print(f'Number of labels: {dataset.labels.shape[1]}')
 
-dataset.view_distributions(percentage=100, variables=['nfp', 'iota'], show=True, overlap=False)
-dataset.view_distributions(percentage=100, variables=['nfp', 'iota'], show=True, overlap=True)
+# -----------------------------------------------------------------------------
+# View the distributions of the dataset
 
-dataset.view_correlations(percentage=100, show=True, method='spearman', variables='features')
-dataset.view_correlations(percentage=100, show=True, method='pearson', variables='features')
-dataset.view_correlations(percentage=100, show=True, method='kendall', variables='features')
+# dataset.view_distributions(percentage=100, variables=['nfp', 'iota'], show=True, overlap=False)
+# dataset.view_distributions(percentage=100, variables=['nfp', 'iota'], show=True, overlap=True)
 
-dataset.calculate_data_counts(IOTA_MIN = 0.2,
+# -----------------------------------------------------------------------------
+# See the correlations between the features and the labels
+
+# dataset.view_correlations(percentage=100, show=True, method='spearman', variables='features')
+# dataset.view_correlations(percentage=100, show=True, method='pearson', variables='features')
+# dataset.view_correlations(percentage=100, show=True, method='kendall', variables='features')
+
+# -----------------------------------------------------------------------------
+# Count the number of samples respecting the restrictions and return a new StellatorsDataSet object with the restricted data
+
+dataset_with_restrictions = dataset.calculate_data_counts(IOTA_MIN = 0.2,
                               MAX_ELONGATION = 10,
                               MIN_MIN_L_GRAD_B = 0.1,
                               MIN_MIN_R0 = 0.3,
@@ -27,7 +33,13 @@ dataset.calculate_data_counts(IOTA_MIN = 0.2,
                               MIN_L_GRAD_GRAD_B = 0.01,
                               MAX_B20_VARIATION = np.inf,
                               MIN_BETA = 1e-4,
-                              MIN_DMERC_TIMES_R2 = 0)
+                              MIN_DMERC_TIMES_R2 = 0,
+                              return_object=True)
+
+# -----------------------------------------------------------------------------
+# View the distributions of the dataset with restrictions applied
+
+dataset_with_restrictions.view_distributions(percentage=100, variables=['nfp', 'iota'], show=True, overlap=False)
 
 
 
