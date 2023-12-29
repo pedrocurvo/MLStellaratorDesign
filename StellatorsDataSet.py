f38fb8cdfa7dc449d8ed5696f105fa167dc0a59a
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2023-12-29 19:01:03 +0000

    Changes in view_distributions method

--- StellatorsDataSet.py
+++ StellatorsDataSet.py
@@ -5,6 +5,7 @@ import pandas as pd
 from typing import Tuple
 import seaborn as sns
 import matplotlib.pyplot as plt
+from tabulate import tabulate
 
 class StellatorsDataSet(Dataset):
     def __init__(self, npy_file, transform=None, normalization=None, sample_size=None):
@@ -66,36 +67,68 @@ class StellatorsDataSet(Dataset):
         labels = torch.tensor(self.data[idx, :10], dtype=torch.float32)
 
         return features, labels
-    
-    def view_distributions(self, variable=None, percentage=100, filename=None, show=True):
-        # Visualize feature distributions using a percentage of the data
+
+
+    def view_distributions(self, variables=None, percentage=100, filename=None, show=True, overlap=False):
+        # Validate inputs
+        if not variables:
+            raise ValueError("No variables provided")
+        if not isinstance(variables, (list, str)):
+            raise TypeError("Variables should be a string or a list of strings")
+        if percentage <= 0 or percentage > 100:
+            raise ValueError("Percentage must be between 0 and 100")
+
+        # Convert to list if variables is a string
+        variables = [variables] if isinstance(variables, str) else variables
+
+        # Calculate the subset size
         subset_size = int((percentage / 100) * len(self.data))
-        #subset_features = pd.DataFrame(self.features[:subset_size, :], columns=[f'col{i+1}' for i in range(self.features.shape[1])])
-        subset_features = pd.DataFrame(self.data[:subset_size, self.data_dict[variable]], columns=[variable])
-        # Replace inf values with NaN
-        # subset_features = subset_features.replace([np.inf, -np.inf, np.nan], 0)
-        plt.figure(figsize=(15, 10))
+
+        # Set plot style
         sns.set(style="whitegrid")
-        try:
-            sns.histplot(subset_features[variable], kde=True)
-        except:
-            sns.histplot(subset_features[variable])
-        plt.title(f"{variable.capitalize()} Distribution ({percentage}% of Data)")
-        # Plot the distributions of the features
-        # for i, column in enumerate(subset_features.columns):
-        #     plt.subplot(3, 3, i + 1)
-        #     sns.histplot(subset_features[column], kde=True)
-        #     plt.title(column)
 
-        plt.tight_layout()
+        # Plot the distribution of the variable
+        if overlap:
+            plt.figure(figsize=(15, 10))
+            for variable in variables:
+                try:
+                    # Plot the distribution of the variable with a kde if continuous
+                    sns.histplot(self.data[:subset_size, self.data_dict[variable]], kde=True)
+                except:
+                    # Plot the distribution of the variable without a kde if discrete
+                    sns.histplot(self.data[:subset_size, self.data_dict[variable]])
+            # Set the title of the plot
+            title = f"{' , '.join(variables).capitalize()} Distribution ({percentage}% of Data)"
+            plt.title(title)
+            plt.tight_layout()
 
-        # Save figure if filename is specified
-        if filename:
-            plt.savefig(filename)
+            # Save figure if filename is specified
+            if filename:
+                plt.savefig(filename)
+            # Show the plot if show is True
+            if show:
+                plt.show()
+        else:
+            for variable in variables:
+                plt.figure(figsize=(15, 10))
+                try:
+                    # Plot the distribution of the variable with a kde if continuous
+                    sns.histplot(self.data[:subset_size, self.data_dict[variable]], kde=True)
+                except:
+                    # Plot the distribution of the variable without a kde if discrete
+                    sns.histplot(self.data[:subset_size, self.data_dict[variable]])
+                # Set the title of the plot
+                plt.title(f"{variable.capitalize()} Distribution ({percentage}% of Data)")
+                plt.tight_layout()
 
-        if show:
-            plt.show()
+                # Save figure if filename is specified
+                if filename:
+                    plt.savefig(filename)
+                # Show the plot if show is True
+                if show:
+                    plt.show()
     
+
     def view_correlations(self, percentage=100, method='pearson', filename=None, show=True, variables='features'):
         # View correlations using a percentage of the data
         subset_size = int((percentage / 100) * len(self.data))
@@ -119,3 +152,66 @@ class StellatorsDataSet(Dataset):
 
         if show:
             plt.show()
+
+
+    def calculate_data_counts(self, IOTA_MIN, MAX_ELONGATION, MIN_MIN_L_GRAD_B, MIN_MIN_R0, MIN_R_SINGULARITY, MIN_L_GRAD_GRAD_B, MAX_B20_VARIATION, MIN_BETA, MIN_DMERC_TIMES_R2):
+        file = self.data
+        # 9 features to check 
+        iota = np.count_nonzero(np.fabs(file[:, 10]) >= IOTA_MIN)
+        max_elongation = np.count_nonzero(file[:, 11] <= MAX_ELONGATION)
+        min_L_grad_B = np.count_nonzero(np.fabs(file[:, 12]) >= MIN_MIN_L_GRAD_B)
+        min_R0 = np.count_nonzero(file[:, 13] >= MIN_MIN_R0)
+        r_singularity = np.count_nonzero(file[:, 14] >= MIN_R_SINGULARITY)
+        L_grad_grad_B = np.count_nonzero(np.fabs(file[:, 15]) >= MIN_L_GRAD_GRAD_B)
+        B20_variation = np.count_nonzero(file[:, 16] <= MAX_B20_VARIATION)
+        beta = np.count_nonzero(file[:, 17] >= MIN_BETA)
+        DMerc_times_r2 = np.count_nonzero(file[:, 18] > MIN_DMERC_TIMES_R2)
+
+        # Percentage of data
+        per_iota = iota/len(file) * 100
+        per_max_elongation = max_elongation/len(file) * 100
+        per_min_L_grad_B = min_L_grad_B/len(file) * 100
+        per_min_R0 = min_R0/len(file) * 100
+        per_r_singularity = r_singularity/len(file) * 100
+        per_L_grad_grad_B = L_grad_grad_B/len(file) * 100
+        per_B20_variation = B20_variation/len(file) * 100
+        per_beta = beta/len(file) * 100
+        per_DMerc_times_r2 = DMerc_times_r2/len(file) * 100
+
+        # Define the data as a list of lists
+        data = [
+            [f'abs(iota) > {IOTA_MIN}', iota, round(per_iota, 2)],
+            [f'max_elongation <= {MAX_ELONGATION}', max_elongation, round(per_max_elongation, 2)],
+            [f'abs(min_L_grad_B) >= {MIN_MIN_L_GRAD_B}', min_L_grad_B, round(per_min_L_grad_B, 2)],
+            [f'min_R0 >= {MIN_MIN_R0}', min_R0, round(per_min_R0, 2)],
+            [f'r_singularity >= {MIN_R_SINGULARITY}', r_singularity, round(per_r_singularity, 2)],
+            [f'abs(L_grad_grad_B) >= {MIN_L_GRAD_GRAD_B}', L_grad_grad_B, round(per_L_grad_grad_B, 2)],
+            [f'B20_variation <= {MAX_B20_VARIATION}', B20_variation, round(per_B20_variation, 2)],
+            [f'beta >= {MIN_BETA}', beta, round(per_beta, 2)],
+            [f'DMerc_times_r2 > {MIN_DMERC_TIMES_R2}', DMerc_times_r2, round(per_DMerc_times_r2, 2)],
+        ]
+
+        # Print the table
+        print(tabulate(data, headers=['Feature', 'Counts', 'Per of Data'], tablefmt='grid'))
+
+        # Count all the data with all restrictions
+        count = np.count_nonzero((np.fabs(file[:, 10]) >= IOTA_MIN) &
+                                (file[:, 11] <= MAX_ELONGATION) &
+                                (np.fabs(file[:, 12]) >= MIN_MIN_L_GRAD_B) &
+                                (file[:, 13] >= MIN_MIN_R0) &
+                                (file[:, 14] >= MIN_R_SINGULARITY) &
+                                (np.fabs(file[:, 15]) >= MIN_L_GRAD_GRAD_B) &
+                                (file[:, 16] <= MAX_B20_VARIATION)
+                                & (file[:, 17] >= MIN_BETA)
+                                & (file[:, 18] > MIN_DMERC_TIMES_R2))
+
+        # Percentage of data
+        per_count = count/len(file) * 100
+
+        # Define the data as a list of lists
+        data = [
+            ['All Restrictions', count, per_count],
+        ]
+
+        # Print the table
+        print(tabulate(data, headers=['Feature', 'Counts', 'Per of Data'], tablefmt='grid'))
