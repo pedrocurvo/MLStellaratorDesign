d87e3aa343f55d81b9d3caf7fab04dd9dd9106d5
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2024-01-16 03:59:06 +0000

    Changed engine.py Added KeyboardInterrupt to train

--- train_pipeline/engine.py
+++ train_pipeline/engine.py
@@ -189,57 +189,62 @@ def train(model: torch.nn.Module,
 
     # Loop through training and testing steps for a number of epochs
     for epoch in tqdm(range(epochs)):
-        train_loss, train_acc = 0, 0
-        test_loss, test_acc = 0, 0
-        if classification:
-            train_metrics = train_loss, train_acc
-            test_metrics = test_loss, test_acc
-        else:
-            train_metrics = train_loss 
-        train_metrics = train_step(model=model,
-                                          dataloader=train_dataloader,
-                                          loss_fn=loss_fn,
-                                          optimizer=optimizer,
-                                          device=device,
-                                          classification=classification)
-        test_metrics = test_step(model=model,
-                                        dataloader=test_dataloader,
-                                        loss_fn=loss_fn,
-                                        device=device,
-                                        classification=classification)
+        try:
+            train_loss, train_acc = 0, 0
+            test_loss, test_acc = 0, 0
+            if classification:
+                train_metrics = train_loss, train_acc
+                test_metrics = test_loss, test_acc
+            else:
+                train_metrics = train_loss 
+            train_metrics = train_step(model=model,
+                                            dataloader=train_dataloader,
+                                            loss_fn=loss_fn,
+                                            optimizer=optimizer,
+                                            device=device,
+                                            classification=classification)
+            test_metrics = test_step(model=model,
+                                            dataloader=test_dataloader,
+                                            loss_fn=loss_fn,
+                                            device=device,
+                                            classification=classification)
 
-        # Print depending on classification or regression
-        if classification:
-            train_loss, train_acc = train_metrics
-            test_loss, test_acc = test_metrics
-            # Print out what's happening
-            print(
-            f"Epoch: {epoch+1} | "
-            f"train_loss: {train_loss:.4f} | "
-            f"train_acc: {train_acc:.4f} | "
-            f"test_loss: {test_loss:.4f} | "
-            f"test_acc: {test_acc:.4f}"
-            )
+            # Print depending on classification or regression
+            if classification:
+                train_loss, train_acc = train_metrics
+                test_loss, test_acc = test_metrics
+                # Print out what's happening
+                print(
+                f"Epoch: {epoch+1} | "
+                f"train_loss: {train_loss:.4f} | "
+                f"train_acc: {train_acc:.4f} | "
+                f"test_loss: {test_loss:.4f} | "
+                f"test_acc: {test_acc:.4f}"
+                )
 
-            # Update results dictionary
-            results["train_loss"].append(train_loss)
-            results["train_acc"].append(train_acc)
-            results["test_loss"].append(test_loss)
-            results["test_acc"].append(test_acc)
-        
-        else:
-            train_loss = train_metrics
-            test_loss = test_metrics
-            # Print out what's happening
-            print(
-            f"Epoch: {epoch+1} | "
-            f"train_loss: {train_loss:.4f} | "
-            f"test_loss: {test_loss:.4f}"
-            )
+                # Update results dictionary
+                results["train_loss"].append(train_loss)
+                results["train_acc"].append(train_acc)
+                results["test_loss"].append(test_loss)
+                results["test_acc"].append(test_acc)
+            
+            else:
+                train_loss = train_metrics
+                test_loss = test_metrics
+                # Print out what's happening
+                print(
+                f"Epoch: {epoch+1} | "
+                f"train_loss: {train_loss:.4f} | "
+                f"test_loss: {test_loss:.4f}"
+                )
 
-            # Update results dictionary
-            results["train_loss"].append(train_loss)
-            results["test_loss"].append(test_loss)
+                # Update results dictionary
+                results["train_loss"].append(train_loss)
+                results["test_loss"].append(test_loss)
+        except KeyboardInterrupt:
+            print("\nKeyboard interrupt detected.")
+            print("Stopping training...")
+            break
 
     # Return the filled results at the end of the epochs
     return results
