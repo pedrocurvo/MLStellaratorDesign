e0728489abf744109525a088ca077620094b5a9d
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2024-01-16 01:34:47 +0000

    Adapt to dataset_up.py Name the models after current Date and Time

--- train.py
+++ train.py
@@ -5,88 +5,41 @@ import os
 from StellatorsDataSet import StellatorsDataSet
 # Measure time
 from timeit import default_timer as timer
+from datetime import datetime
 import torch.nn as nn
-from train_pipeline import engine, model_builder, utils
+from train_pipeline import engine, model_builder, utils, data_setup
 
 # Important for num_workers > 0
 if __name__ == "__main__":
+    # Get Current Date and Time to name the model
+    now = datetime.now()
+    current_date = now.strftime("%Y_%m_%d_%H_%M_%S")
+
     # Dataset
     # Load the data
-    full_dataset = StellatorsDataSet(npy_file='data/dataset.npy')
-
-    full_dataset = full_dataset.calculate_data_counts(IOTA_MIN = 0.2,
-                                MAX_ELONGATION = 10,
-                                MIN_MIN_L_GRAD_B = 0.1,
-                                MIN_MIN_R0 = 0.3,
-                                MIN_R_SINGULARITY = 0.05,
-                                MIN_L_GRAD_GRAD_B = 0.01,
-                                MAX_B20_VARIATION = 5,
-                                MIN_BETA = 1e-4,
-                                MIN_DMERC_TIMES_R2 = 0,
-                                return_object=True)
+    full_dataset = StellatorsDataSet(npy_file='data/dataset.npy', sample_size=90000)
 
     # Setup device-agnostic code 
     if torch.cuda.is_available():
         device = "cuda" # NVIDIA GPU
-    elif torch.backends.mps.is_available():
-        device = "mps" # Apple GPU
+    # elif torch.backends.mps.is_available():
+    #     device = "mps" # Apple GPU
     else:
         device = "cpu" # Defaults to CPU if NVIDIA GPU/Apple GPU aren't available
 
     print(f"Using device: {device}")
 
     # Setup the Hyperparameters
-    BATCH_SIZE = len(full_dataset) // 10
+    BATCH_SIZE = 64
     NUM_EPOCHS = 10
-    LEARING_RATE = 0.01
-
-    # Define sizes for train, validation, and test sets
-    train_size = int(0.5 * len(full_dataset))
-    val_size = int(0.1 * len(full_dataset))
-    test_size = len(full_dataset) - train_size - val_size
-
-    # Split the dataset
-    train_dataset, val_dataset, test_dataset = random_split(full_dataset, [train_size, val_size, test_size])
-
-    # Normalize the data
-    train_dataset.transform = transforms.Compose([
-        transforms.ToTensor(),
-    ])
-
-    avg_features = torch.mean(train_dataset[:][0], dim=0)
-    std_features = torch.std(train_dataset[:][0], dim=0)
-
-    print(f'avg: {avg_features}')
-    print(f'std: {std_features}')
-
-    # Normalize the data
-    # Normalize everything with the average and standard deviation of the training set
-    # This is important to avoid data leakage
-    train_dataset.transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(avg_features, std_features)
-    ])
-
-    test_dataset.transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(avg_features, std_features)
-    ])
-    
+    LEARING_RATE = 0.1
 
     # Turn datasets into iterable objects (batches)
-    train_loader = DataLoader(dataset=train_dataset, # dataset to turn into iterable batches
-                            batch_size=BATCH_SIZE, # samples per batch
-                            shuffle=True, # shuffle data (for training set only)
-                            num_workers=4, # subprocesses to use for data loading
-                            pin_memory=True # CUDA only
-    ) 
-
-    test_loader = DataLoader(dataset=test_dataset,
-                            batch_size=BATCH_SIZE,
-                            shuffle=False,
-                            num_workers=4,
-                            pin_memory=True
-    )
+    # Create DataLoaders with help from data_setup.py
+    train_dataloader, test_dataloader = data_setup.create_dataloaders(dataset=full_dataset,
+                                                                    train_size=0.7,
+                                                                    batch_size=BATCH_SIZE
+)
 
     # Create the model from the model_builder.py
     model = model_builder.Model(input_dim=9,
@@ -100,12 +53,12 @@ if __name__ == "__main__":
 
     # Set the seed and start the timer 
     train_time_start_on_gpu = timer()
-
+    print(f"Training on {device}...")
 
     # Start the training session with engine.py
     engine.train(model=model,
-                train_dataloader=train_loader,
-                test_dataloader=test_loader,
+                train_dataloader=train_dataloader,
+                test_dataloader=test_dataloader,
                 optimizer=optimizer,
                 loss_fn=loss_fn,
                 epochs=NUM_EPOCHS,
@@ -122,4 +75,4 @@ if __name__ == "__main__":
     # Save the model with help from utils.py
     utils.save_model(model=model,
                     target_dir="models",
-                    model_name=f"{model.__class__.__name__}.pth")
+                    model_name=f"{model.__class__.__name__}_{current_date}.pth")
