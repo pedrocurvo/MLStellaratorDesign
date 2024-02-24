2dfb7caa6e81584731071e38f20b82a8e72105a6
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2024-02-24 19:26:57 +0000

    validate_args=False

--- model.py
+++ model.py
@@ -14,9 +14,6 @@ from tensorflow_probability.python.bijectors import FillScaleTriL
 # -----------------------------------------------------------------------------
 
 def create_model(input_dim, output_dim):
-    validate_args = False
-    allow_nan_stats = False
-
     model = Sequential()
 
     # neural network
@@ -37,8 +34,8 @@ def create_model(input_dim, output_dim):
     model.add(DistributionLambda(lambda t: Mixture(
         # parameterized categorical for component selection
         cat=Categorical(logits=t[...,:K],
-                        validate_args=validate_args,
-                        allow_nan_stats=allow_nan_stats),
+                        validate_args=False,
+                        allow_nan_stats=True),
         # parameterized components
         components=[MultivariateNormalTriL(
             # parameterized mean of each component
@@ -46,10 +43,10 @@ def create_model(input_dim, output_dim):
             # parameterized covariance of each component
             scale_tril=FillScaleTriL().forward(
                 t[...,K+i*params_size+loc_size:K+i*params_size+loc_size+scale_size]),
-            validate_args=validate_args,
-            allow_nan_stats=allow_nan_stats) for i in range(K)],
-        validate_args=validate_args,
-        allow_nan_stats=allow_nan_stats)))
+            validate_args=False,
+            allow_nan_stats=True) for i in range(K)],
+        validate_args=False,
+        allow_nan_stats=True)))
 
     # optimizer, learning rate, and loss function
     opt = Adam(1e-4)
