9014c2455c3ed7f445e8defff88b994ea6917eb0
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2024-03-01 00:44:42 +0000

    fix: update MBuilder __init__.py

--- train_pipeline/MBuilder/__init__.py
+++ train_pipeline/MBuilder/__init__.py
@@ -1,3 +1,6 @@
 from .MixtureDensityNetwork import MixtureDensityNetwork
 from .ForwardNeuralNetwork import ForwardNeuralNetwork
 from .MDN_FCOV import MDN_FCOV
+from .VAE import VAE
+from .Encoder import Encoder
+from .Decoder import Decoder
