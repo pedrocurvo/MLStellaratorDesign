f28ee7b68c1b879c4a734045c4e7aec2c5b4ab7a
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2024-03-27 22:13:07 +0000

    histograms

--- mdn_keras/histograms.py
+++ mdn_keras/histograms.py
@@ -18,29 +18,24 @@ for col in df_0.columns:
     values_0 = df_0[col].values
     values_1 = df_1[col].values
 
-    if col == 'nfp':
+    if col in ['nfp']:
         vmin = np.min(values_0) - 0.5
         vmax = np.max(values_0) + 0.5
         num = int(round(vmax - vmin + 1))
 
-    elif col in ['iota',
-                 'r_singularity',
-                 'min_L_grad_B']:
+    elif col in ['iota', 'r_singularity', 'min_L_grad_B']:
         p = np.percentile(np.fabs(values_0), 80)
         vmin = max(-p, np.min(values_0))
         vmax = min(+p, np.max(values_0))
         num = 801
 
-    elif col in ['max_elongation',
-                 'L_grad_grad_B']:
+    elif col in ['max_elongation', 'L_grad_grad_B']:
         p = np.percentile(np.fabs(values_0), 50)
         vmin = max(-p, np.min(values_0))
         vmax = min(+p, np.max(values_0))
         num = 501
 
-    elif col in ['B20_variation',
-                 'beta',
-                 'DMerc_times_r2']:
+    elif col in ['B20_variation', 'beta', 'DMerc_times_r2']:
         p = np.percentile(np.fabs(values_0), 10)
         vmin = max(-p, np.min(values_0))
         vmax = min(+p, np.max(values_0))
