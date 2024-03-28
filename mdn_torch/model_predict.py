406c4bcef9a949abecdef107a11e6baeec357610
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-03-28 22:44:11 +0000

    predictions: updated predictions file

--- mdn_torch/model_predict.py
+++ mdn_torch/model_predict.py
@@ -1,31 +1,14 @@
-import torch
-import signal
-from tqdm import tqdm
 import os
+import torch
+from tqdm import tqdm
 import numpy as np
-
-from StellaratorDataSet import StellaratorDataSetInverse
-# Measure time
-from timeit import default_timer as timer
-from datetime import datetime
-from train_pipeline import engine, utils, data_setup
+import pandas as pd
 from MDNFullCovariance import MDNFullCovariance
+from preditcions_utils import sample_output, check_criteria, run_qsc, round_nfp
+
 
 # -----------------------------------------------------------------------------
-
-def ignore_sigint():
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
-
-cpus = os.cpu_count()
-
-# -----------------------------------------------------------------------------
-
-# Important for num_workers > 0
 if __name__ == "__main__":
-    # Dataset
-    # Load the data
-    full_dataset = StellaratorDataSetInverse(npy_file='../data/dataset.npy')
-
     # Setup device-agnostic code 
     if torch.cuda.is_available():
         device = "cuda" # NVIDIA GPU
@@ -33,21 +16,9 @@ if __name__ == "__main__":
         device = "cpu" # Defaults to CPU if NVIDIA GPU/Apple GPU aren't available
 
     print(f"Using device: {device}")
-
-    # Setup the Hyperparameters
-    BATCH_SIZE = 500
-    NUM_OF_WORKERS = os.cpu_count()
-
-    # Turn datasets into iterable objects (batches)
-    # Create DataLoaders with help from data_setup.py
-    train_dataloader, val_dataloader, test_dataloader, mean_std = data_setup.create_dataloaders(dataset=full_dataset,
-                                                                    train_size=0.5,
-                                                                    val_size=0.2,
-                                                                    batch_size=BATCH_SIZE,
-                                                                    num_workers=NUM_OF_WORKERS
-)
-
-
+    
+    # Load mean_std from a file
+    mean_std = torch.load("models/mean_std.pth")
 
     # Create model
     model = MDNFullCovariance(input_dim=10,
@@ -58,13 +29,12 @@ if __name__ == "__main__":
     # Load a previous model (optional: uncomment if you want to load a previous model): transfer learning
     model.load_state_dict(torch.load("models/MDNFullCovariance/2024_03_28_11_53_42.pth"))
 
-    # Sample from .csv 
-    import pandas as pd
-    from preditcions_utils import sample_output, check_criteria, run_qsc, round_nfp
-
+    # -----------------------------------------------------------------------------
+    # Load dataset with only good stellarators to generate samples
     df = pd.read_csv('./data_good/good_stellarators_dataset_first.csv')
 
-
+    # -----------------------------------------------------------------------------
+    # File to keep the new predictions
     fname = './dump/predict_first.csv'
     print('Writing:', fname)
 
@@ -73,6 +43,9 @@ if __name__ == "__main__":
     else:
         f = open(fname, 'w')
         print(','.join(df.columns), file=f)
+    
+    # -----------------------------------------------------------------------------
+    # Predict
 
     it = 0
     progress_bar = tqdm(
@@ -90,24 +63,21 @@ if __name__ == "__main__":
             # Transform into tensor
             sample = torch.tensor(sample).float().to(device).unsqueeze(0)
 
-            # Remove mean and divide by std
+            # Remove mean and divide by std to normalize
             sample = (sample - mean_std["mean"].to(device)) / mean_std["std"].to(device)
 
             # Pass through model
-            output = model.getMixturesSample(sample, device)
+            sample = model.getMixturesSample(sample, device)
 
             # Add mean and multiply by std
-            output = output * mean_std["std_labels"].to(device) + mean_std["mean_labels"].to(device)
-
-            # Round the 7th element of the output
-            output[0][6] = torch.round(output[0][6])
+            sample = sample * mean_std["std_labels"].to(device) + mean_std["mean_labels"].to(device)
 
             # Run qsc
-            output = output.cpu().numpy()
+            sample = sample.cpu().numpy()
 
             try: 
-                output[0] = round_nfp(output[0])
-                qsc_values = run_qsc(output[0])
+                sample[0] = round_nfp(sample[0])
+                qsc_values = run_qsc(sample[0])
 
                 # Check criteria
                 if check_criteria(qsc_values):
@@ -117,7 +87,7 @@ if __name__ == "__main__":
                 assert not np.isinf(qsc_values).any()
                 assert not (np.fabs(qsc_values) > np.finfo(np.float32).max).any()
 
-                values = np.concatenate([output[0], qsc_values], dtype=str)
+                values = np.concatenate([sample[0], qsc_values], dtype=str)
                 print(','.join(values), file=f)
 
             except Warning:
