db4103c1ad73ece3cb7ab1a4321c56bd07e7037f
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-04-05 00:55:07 +0000

    update select_good_stellarators.py

--- mdn_torch/select_good_stellarators.py
+++ mdn_torch/select_good_stellarators.py
@@ -2,45 +2,27 @@ import modin.pandas as pd
 import numpy as np
 
 # Load the dataset
-df = pd.read_csv('../data/fifth_dataset.csv')
-
-# Find NaN values in the dataset
-# data = np.load('../data/fifth_dataset.npy')
-# df = pd.DataFrame(data)
-# df.to_csv('../data/fifth_dataset.csv', index=False, header=df_4.columns)
+df = pd.read_csv('../data/sixth_dataset.csv')
 
 # Print the shape of the dataset
 print(f"Shape of the dataset: {df.shape}")
 
-# Select the rows with axis_length > 0
-df = df[df['axis_length'] > 0]
+# Conditions for a good stellarator
+condition = (
+    (df['axis_length'] > 0) &
+    (np.fabs(df['iota']) >= 0.2) &
+    (df['max_elongation'] <= 10) &
+    (np.fabs(df['min_L_grad_B']) >= 0.1) &
+    (np.fabs(df['min_R0']) >= 0.3) &
+    (df['r_singularity'] >= 0.05) &
+    (np.fabs(df['L_grad_grad_B']) >= 0.1) &
+    (df['B20_variation'] <= 5) &
+    (df['beta'] >= 1e-4) &
+    (df['DMerc_times_r2'] > 0)
+)
 
-# Select the rows with |iota| >= 0.2
-df = df[np.fabs(df['iota']) >= 0.2]
-
-# Select the rows with max_elongation <= 10
-df = df[df['max_elongation'] <= 10]
-
-# Select the rows with |min_L_grad_B| >= 0.1
-df = df[np.fabs(df['min_L_grad_B']) >= 0.1]
-
-# Select the rows with |min_R0| >= 0.3
-df = df[np.fabs(df['min_R0']) >= 0.3]
-
-# Select the rows with r_singularity >= 0.05
-df = df[df['r_singularity'] >= 0.05]
-
-# Select the rows with |L_grad_grad_B| >= 0.1
-df = df[np.fabs(df['L_grad_grad_B']) >= 0.1]
-
-# Select the rows with B20_variation <= 5
-df = df[df['B20_variation'] <= 5]
-
-# Select the rows with beta >= 1e-4
-df = df[df['beta'] >= 1e-4]
-
-# Select the rows with DMerc_times_r2 > 0
-df = df[df['DMerc_times_r2'] > 0]
+# Select rows that satisfy the combined condition
+df = df[condition]
 
 # Display the first few rows of the filtered dataset
 print(df.head())
@@ -49,6 +31,6 @@ print(df.head())
 print(f"Shape of the filtered dataset: {df.shape}")
 
 # Save the filtered dataset to a new CSV file
-df.to_csv('./data_good/fifth_good_stels.csv', index=False)
+df.to_csv('./data_good/sixth_good_stels.csv', index=False)
 
 
